f8f54294c0b524cf638f80f166639b290052fef4
yuliemalavez <yuliemalavez@outlook.com>
2015-07-14 20:57:09 +0000

    Create new and add forms and parcial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get '/home', to: 'pages#home'
+  
+  
+  #get '/recipes', to: 'recipes#index'
+  #post '/recipes', to: 'recipes#create'
+  
+  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edith_recipe'
+  
+  #patch '/recipes/:id', to: 'recipes#update'
+  #get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+  
+  #delete '/recipes/:id', to: 'recipes#destroy'
+  
+  
+  resources :recipes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
